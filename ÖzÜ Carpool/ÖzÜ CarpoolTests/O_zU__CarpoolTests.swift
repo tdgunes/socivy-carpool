0122e71b5b32c669b4e6170910e2658444d2c849
Taha Doğan Güneş <tdgunes@gmail.com>
2014-09-24 00:30:04 +0300

    Add initial iOS App

---
+++ ÖzÜ Carpool/ÖzÜ CarpoolTests/O_zU__CarpoolTests.swift
@@ -0,0 +1,36 @@
+//
+//  O_zU__CarpoolTests.swift
+//  ÖzÜ CarpoolTests
+//
+//  Created by Taha Doğan Güneş on 24/09/14.
+//  Copyright (c) 2014 TDG. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class O_zU__CarpoolTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
