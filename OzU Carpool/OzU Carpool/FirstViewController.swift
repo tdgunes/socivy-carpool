d3d5997721abd3bcd891b91900e5db3a937559d8
Taha Doğan Güneş <tdgunes@gmail.com>
2014-09-24 01:08:03 +0300

    Change the name, add user related views

--- OzU Carpool/OzU Carpool/FirstViewController.swift
+++ OzU Carpool/OzU Carpool/FirstViewController.swift
@@ -1,6 +1,6 @@
 //
-//  SecondViewController.swift
-//  ÖzÜ Carpool
+//  FirstViewController.swift
+//  OzU Carpool
 //
 //  Created by Taha Doğan Güneş on 24/09/14.
 //  Copyright (c) 2014 TDG. All rights reserved.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- OzU Carpool/OzU Carpool/SecondViewController.swift
+++ OzU Carpool/OzU Carpool/SecondViewController.swift
@@ -1,6 +1,6 @@
 //
-//  FirstViewController.swift
-//  ÖzÜ Carpool
+//  SecondViewController.swift
+//  OzU Carpool
 //
 //  Created by Taha Doğan Güneş on 24/09/14.
 //  Copyright (c) 2014 TDG. All rights reserved.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
