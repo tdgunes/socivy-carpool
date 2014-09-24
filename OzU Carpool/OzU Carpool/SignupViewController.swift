ffa23bc714af8bff6a84382455603198881cec4a
Taha Doğan Güneş <tdgunes@gmail.com>
2014-09-24 03:31:26 +0300

    Add connections of SignupViewController and delete First and Second ViewControllers

--- OzU Carpool/OzU Carpool/SignupViewController.swift
+++ OzU Carpool/OzU Carpool/SignupViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 
 class SignupViewController: UITableViewController {
     
+    @IBOutlet weak var nameSurnameCell:TextFieldCell?
+    @IBOutlet weak var emailCell:TextFieldCell?
+    @IBOutlet weak var passwordCell:TextFieldCell?
+    @IBOutlet weak var phoneNumberCell:TextFieldCell?
+    @IBOutlet weak var registerCell:UITableViewCell?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,5 +32,25 @@ class SignupViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        var selectedCell = self.tableView.cellForRowAtIndexPath(indexPath)
+        if selectedCell == registerCell {
+            self.register()
+        }
+        
+        
+    }
+    
+    func register(){
+        println("[peek] register touched")
+        println("[peek] name: \(nameSurnameCell?.textField?.text)")
+        println("[peek] email: \(emailCell?.textField?.text)")
+        println("[peek] password: \(passwordCell?.textField?.text)")
+        println("[peek] phoneNumber: \(phoneNumberCell?.textField?.text)")
+        
+        
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
     
 }
