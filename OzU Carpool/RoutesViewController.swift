60fced04c15f9c911b0615dcf8cccadfd771e566
Taha Doğan Güneş <tdgunes@gmail.com>
2014-09-24 04:33:34 +0300

    Change the structure of the app

---
+++ OzU Carpool/RoutesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RoutesViewController.swift
+//  OzU Carpool
+//
+//  Created by Taha Doğan Güneş on 24/09/14.
+//  Copyright (c) 2014 TDG. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RoutesViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
