8401377400ce83f6627531a3d614ea538b6a68ac
Ömer Kala <kalaomer@hotmail.com>
2014-09-26 02:35:16 +0300

    Google Analytics düzenleme

--- www/resources/views/layouts/master.blade.php
+++ www/resources/views/layouts/master.blade.php
@@ -39,10 +39,10 @@
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            ga('create', 'UA-55177652-1', 'auto');
+            ga('create', 'UA-55177652-2', 'auto');
             ga('send', 'pageview');
         </script>
-        
+
         @yield('head-end')
     </head>
     <body>
