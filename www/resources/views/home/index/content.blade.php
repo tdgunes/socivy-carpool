718ee9ffe04aec82e7902e5ab0b51b96be1dd606
Ömer Kala <kalaomer@hotmail.com>
2014-09-26 03:43:14 +0300

    Home page eklendi

---
+++ www/resources/views/home/index/content.blade.php
@@ -0,0 +1,11 @@
+
+<div class="row">
+    <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+        <p class="main-title">
+            Socivy Carpool
+        </p>
+        <p class="main-text">
+            Özyeğin Üniversitesi >araç paylaşım ağı!
+        </p>
+    </div>
+</div>
