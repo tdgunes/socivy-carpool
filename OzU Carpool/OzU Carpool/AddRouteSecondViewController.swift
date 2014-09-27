0776adc57b3d0a82a8be60d27912de4237e561c6
Taha Doğan Güneş <tdgunes@gmail.com>
2014-09-27 18:57:26 +0300

    Change AddRouteSecondViewController, add configureView()

--- OzU Carpool/OzU Carpool/AddRouteSecondViewController.swift
+++ OzU Carpool/OzU Carpool/AddRouteSecondViewController.swift
@@ -20,9 +20,68 @@ class AddRouteSecondViewController: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
+        self.configureMapView()
         self.mapView?.delegate = self
     }
+    
+    
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        
+        var optionalPinView:MKPinAnnotationView? = mapView.dequeueReusableAnnotationViewWithIdentifier("pinView") as MKPinAnnotationView?
+        
+        if var pinView = optionalPinView {
+            pinView.annotation = annotation
+            return pinView
+        }
+        else {
+            var newPinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinView")
+            
+            if annotation is Location{
+                newPinView.pinColor = (annotation as Location).color
+                newPinView.image = UIImage(named: "16-car")
+                newPinView.animatesDrop = false
+                newPinView.canShowCallout = true
+                var rightButton = UIButton.buttonWithType(UIButtonType.ContactAdd) as UIButton
+                
+                newPinView.rightCalloutAccessoryView = rightButton
+                
+                
+            }
+            else {
+                
+                
+                newPinView.pinColor = MKPinAnnotationColor.Red
+                newPinView.animatesDrop = false
+                newPinView.canShowCallout = true
+                
+//                details button
+                            var rightButton = UIButton.buttonWithType(UIButtonType.ContactAdd) as UIButton
+                
+                            newPinView.rightCalloutAccessoryView = rightButton
+                
+                
+            }
+            return newPinView
+        }
+        
+        
+        
+        
+    }
+    
+    
+    func configureMapView(){
+        var recognizer:UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "handleLongPress:")
+        recognizer.minimumPressDuration = 1.5
+        
+        self.mapView?.addGestureRecognizer(recognizer)
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude:41.030420 , longitude: 29.122009), span: MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3))
+        self.mapView?.setRegion(region, animated: true)
+        
+        self.mapView?.addAnnotations(LocationStorage.sharedInstance.getAll() as [Location])
+    }
+    
     
     
     override func didReceiveMemoryWarning() {
